b656674437bd243a56dbfbe73458f9a6d4681419
Adithyan.S <adhi09791@gmail.com>
2026-06-08 18:06:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menuIcon.onclick =() =>{
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+};
 
-menuIcon.classList.toggle('bx-x')
-navbar.classList.toggle('active');
-}
+// --- ADD THIS NEW CODE BELOW ---
+
+// Select all the links inside the navbar
+let navLinks = document.querySelectorAll('.navbar a');
+
+// Tell each link to close the menu when clicked
+navLinks.forEach(link => {
+  link.onclick = () => {
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+  };
+});
